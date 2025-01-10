8cf5b8d205dc3641387edcdd0878670615cb0272
Ocelot78 <wojtekkonieczny1@gmail.com>
2025-01-10 17:10:50 +0100

    readlist and files (not finished)

--- Window Version/app/js/script.js
+++ Window Version/app/js/script.js
@@ -4,17 +4,15 @@ function addList(name) {
     const emptylist = {};
     const data = JSON.stringify(emptylist);
     const filePath = path.join("app/js","data", name+".json")
-    fs.access(filePath, fs.constans.F_OK, (err) => {
-        if (err) {
-            throw err
-        } else {
-            fs.writeFile(filePath, data, (error) => {
-                if (error){
-                    throw error
-                }
-            })
-        }
-    })
+    if (window.fs.fileExists(filePath))
+        throw (error)
+    else {
+        fs.writeFile(filePath, data, (error) => {
+            if (error){
+                throw error
+            }
+        })
+    }
     button.id = name;
     button.innerHTML = `
         <img src="assets/medium.svg" alt="">
@@ -22,17 +20,55 @@ function addList(name) {
         ${name}`
     lists.appendChild(button);
 }
-
+function readlists() {
+    const lists = document.getElementById("lists");
+    const filePath = "app/js/data/"
+    let fileslist = fs.readdirSync(filePath)
+    for (let i in fileslist) {
+        fs.readFile(filePath+fileslist[i], (error, data) => {
+            if (error) {
+                throw (error)
+            }
+            console.log(fileslist[i])
+            let name = fileslist[i] 
+            name = name.replace(".json","")
+            const button = document.createElement("button");
+            button.id = name;
+            button.innerHTML = `
+                <img src="assets/medium.svg" alt="">
+                <h1>|</h1>
+                ${name}`
+            lists.appendChild(button); 
+        })
+    }
+}
+function readlist(name,folder) {
+    const filePath = "app/js/"+folder+"/"+name+".json"
+    fs.readFile(filePath,"utf-8", (error,data)=>{
+        if (error) {
+            throw error
+        }
+        const tasks = JSON.parse(data)
+        console.log(tasks)
+    })
+}
+readlist("routine","default")
+function appearList(name, data) {
+    let listButton = document.getElementById(name+".json")
+}
 document.addEventListener("DOMContentLoaded", () => {
     let add = document.getElementById("newList");
+    let app = document.getElementById("app")
     let alert = document.getElementById("inputAlert");
     let listInput = document.getElementById("listInput")
     let alertAccept = document.getElementById("inputAlertAccept");
     let alertCancel = document.getElementById("inputAlertCancel");
     let errorText = document.getElementById("errorAlert")
+    readlists()
     add.addEventListener("click", () => {
-        alert.style.display = 'flex'
+        alert.style.display = "flex"
         errorText.innerHTML = ""
+        app.style.filter = "blur(8px)";
     });
     alertAccept.addEventListener("click", () => {
         if (listInput.value == ""){
@@ -40,12 +76,16 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             try {
                 addList(listInput.value)
-            }catch (err){
+                alert.style.display = "none"
+                app.style.filter = "none";
+            }catch (error){
                 errorText.innerHTML = "Błąd podczas tworzenia pliku Sprawdz czy plik nie istnieje."
             }   
         }
+        
     })
     alertCancel.addEventListener("click", () => {
         alert.style.display = "none"
+        app.style.filter = "none";
     })
 });
--- Window Version/preload.js
+++ Window Version/preload.js
@@ -2,9 +2,19 @@ const {contextBridge} = require("electron")
 const fs = require("fs");
 const path = require("path");
 contextBridge.exposeInMainWorld('fs', {
+    readFile: (option1, option2,option3) => fs.readFile(option1,option2,option3),
     writeFile: (option1,option2,option3)  => fs.writeFile(option1,option2,option3),
     access: (option1,option2,option3) => fs.access(option1,option2,option3),
+    readdirSync: (option) => fs.readdirSync(option),
     constants: fs.constants,
+    fileExists: (filePath) => {
+      try {
+        fs.accessSync(filePath, fs.constants.F_OK);
+        return true;
+      } catch {
+        return false;
+      }
+    }
 });
 contextBridge.exposeInMainWorld('path', {
     join: (...args) => path.join(...args),
